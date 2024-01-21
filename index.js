2a9632347351409aeacf3b3e7b96e44b574f7145
TSezari Kvelashvili <c.kvelashvili@gmail.com>
2024-01-21 14:33:47 +0400

    updated server

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const { Server } = require('socket.io')
+const http = require('http')
+const cors = require('cors')
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const server = http.createServer(app)
+
+let onlineUsers = 0
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+})
+
+// middleware
+app.use(cors())
 
 // connect to database
+const PORT = process.env.PORT || 5000
 mongoose.connect(process.env.dbURI)
     .then(() => {
-        app.listen(PORT, () => console.log(`listening for requests on port ${PORT}`))
+        server.listen(PORT, () => console.log(`listening for requests on port ${PORT}`))
     })
 
 // routes
 app.get('/', (req, res) => {
     res.status(200).json({success: true})
+})
+
+// socket.io
+io.on('connection', (socket) => {
+    console.log('connected', socket.id)
+    // onlineUsers++
+    // io.emit('online_users', onlineUsers)
+
+    // socket.on('disconnect', (socket) => {
+    //     onlineUsers--
+    //     io.emit('online_users', onlineUsers)
+    // })
 })
